813abb17e994c9b50786bb1e36916ee7c4df1f9b
Narendran Elango <41041022+Narendranelango@users.noreply.github.com>
2021-08-14 15:04:45 +0530

    Combining Reducers

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -18,6 +18,7 @@ import React from 'react';
     //     );
     // }
 
+    
     function RenderMenuItem ({dish, onClick}) {
         return (
             <Card>
---
+++ src/redux/comments.js
@@ -0,0 +1,9 @@
+import { COMMENTS } from '../shared/comments';
+
+export const Comments = (stat = COMMENTS, action) => {
+    switch (action.type) {
+
+        default:
+          return stat;
+      }
+};
